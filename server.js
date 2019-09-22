64f4afde49429d6a5945ccd6f7f4fb5fc7a2df6b
Allen Guan <apguan@gmail.com>
2019-09-22 16:29:48 -0700

    adding socket namespaces

--- server.js
+++ server.js
@@ -7,13 +7,40 @@ const io = require("socket.io")(http);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-io.on("connection", function(socket) {
+app.get("/:roomId", (req, res) => {
   console.log("a user connected");
+  let room = req.params.roomId;
+
+  const nsp = io.of(room);
+
+  let queue = [];
+  let messages = [];
+  let videoDetails = {};
+
+  // nsp.on("connect", socket => {
+  //   console.log("someone has connected to the room");
+  // });
+
+  // nsp.on("queue", socket => {
+  //   console.log(socket);
+  // });
+
+  // nsp.on("room", socket => {
+  //   console.log(socket);
+  // });
+
+  // nsp.on("video", socket => {
+  //   console.log(socket);
+  // });
+
+  nsp.emit("hi", "hello world");
+  nsp.emit("queue");
 });
-http.listen(4000, function() {
+
+http.listen(4000, () => {
   console.log("listening on *:4000");
 });
