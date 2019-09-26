34270e741a7fdddc6ca44d29231853a1fe39a7ca
Allen Guan <apguan@gmail.com>
2019-09-25 22:19:24 -0700

    changing port

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -2,9 +2,12 @@ import { useState } from "react";
 import socketIOClient from "socket.io-client";
 
 let connectSocket = function(room) {
-  return socketIOClient("http://localhost:4000", {
-    query: "r_var=" + room
-  });
+  return socketIOClient(
+    "ws://watch-youtube-with-me.herokuapp.com/socket.io/?EIO=4&transport=websocket",
+    {
+      query: "r_var=" + room
+    }
+  );
 };
 
 export const Sockets = roomId => {
